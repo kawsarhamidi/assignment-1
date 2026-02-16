889b890a683b1eca2a952e5aa0764a65b376f672
kawsarhamidi <kawsarhamidi77@gmail.com>
2026-02-16 19:27:32 +0900

    try display sow the products

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const loadProducts = () => {
     fetch("https://fakestoreapi.com/products")
   .then(res => res.json())
-  .then((json) => console.log(json))
+  .then((json) => displayProduct(json));
 
 };
 
 const displayProduct = (products) =>{
-    console.log(products);
+    const productsContainer = document.getElementById("products-container");
+    productsContainer.innerHTML = "";
+    for(let product of product){
+        const  btnDiv = document.createElement("div");
+        btnDiv.innerHTML = ``;
+
+        productsContainer.append(btnDiv);
+    }
     
 };
 
